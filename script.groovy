cb3bfd505bd898f76c45197fb6048ecc1513345f
AbhishekDevani <GS-3257@GSLAB.COM>
2023-09-11 14:42:23 +0530

    create folder

--- script.groovy
+++ script.groovy
@@ -1,7 +1,18 @@
-def initApp() {
+// def initApp() {
 
-    echo "Initiating the application"
+//     echo "Initiating the application"
 
+// }
+
+// return this
+
+// Import necessary libraries
+import java.io.File
+
+// Define a function to create a folder on the desktop
+def createFolderOnDesktop(folderName) {
+    def desktopPath = System.getProperty("user.home") + "/Desktop"
+    def folderPath = desktopPath + "/" + folderName
+    def folder = new File(folderPath)
+    folder.mkdirs()
 }
-
-return this
